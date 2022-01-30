e9b5b5dcd03df6e46865e58146a4816179ce8d2b
mokrooussovi <ivan.mokrooussov@dcmail.ca>
2022-01-30 17:44:05 -0500

    added contact form

--- Scripts/app.js
+++ Scripts/app.js
@@ -15,8 +15,8 @@
         //let DocumentBody = document.body.style.backgroundImage;
         document.body.style.backgroundImage = "url(view3.png)";
         document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundSize = "1670px 1600px";
-        document.body.style.backgroundPosition = "left";
+        document.body.style.backgroundSize = "930px 1500px";
+        document.body.style.backgroundPosition = "center right";
     }
 
     function DisplayProductsPage()
@@ -27,27 +27,7 @@
         document.body.style.backgroundImage = "url(view4.png)";
         document.body.style.backgroundRepeat = "no-repeat";
         document.body.style.backgroundSize = "1670px 1600px";
-        document.body.style.backgroundPosition = "center right";
-        
-        // Step 1. get an entry point(s) (insertion point / deletion point) reference 
-        let DocumentBody = document.body;
-        let MainContent = document.getElementsByTagName("main")[0];
-
-        // Step 2. create an element(s) to insert
-        let MainParagraph = document.createElement("p");
-        let Article = document.createElement("article");
-        let ArticleParagraph = `<p id="ArticleParagraph" class ="mt-3">Details on favourite projects</p>`
-
-        // Step 3. configure the new element
-        MainParagraph.setAttribute("id", "MainParagraph");
-        MainParagraph.setAttribute("class", "mt-3");
-        MainParagraph.textContent = "WEBD6201 JavaScript exercise allows to create contact's list with use of localStorage";
-        Article.setAttribute("class", "container");
-
-        // Step 4. Add / Insert the new element
-        MainContent.appendChild(MainParagraph);
-        Article.innerHTML = ArticleParagraph;
-        DocumentBody.appendChild(Article);
+        document.body.style.backgroundPosition = "top left";
  
     }
 
@@ -56,9 +36,9 @@
         console.log("Services Page");
 
         //let DocumentBody = document.body.style.backgroundImage;
-        document.body.style.backgroundImage = "url(view2.png)";
+        document.body.style.backgroundImage = "url(view7.png)";
         document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundSize = "1670px 1600px";
+        document.body.style.backgroundSize = "1100px 800px";
         document.body.style.backgroundPosition = "top right";
         
 
@@ -69,10 +49,11 @@
         console.log("About Page");
 
         //let DocumentBody = document.body.style.backgroundImage;
-        document.body.style.backgroundImage = "url(view8.png)";
+        document.body.style.backgroundImage = "url(view10.png)";
         document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundSize = "1670px 1600px";
-        document.body.style.backgroundPosition = "right";
+        document.body.style.backgroundSize = "400px 600px";
+        document.body.style.backgroundPosition = "100px 300px";
+
         
     }
 
@@ -81,7 +62,7 @@
         console.log("Human Resources Page");
 
         //let DocumentBody = document.body.style.backgroundImage;
-        document.body.style.backgroundImage = "url(view9.png)";
+        document.body.style.backgroundImage = "url(view2.png)";
         document.body.style.backgroundRepeat = "no-repeat";
         document.body.style.backgroundSize = "1670px 1600px";
         document.body.style.backgroundPosition = "right";
@@ -92,10 +73,65 @@
         console.log("Contact Page");
 
         //let DocumentBody = document.body.style.backgroundImage;
-        document.body.style.backgroundImage = "url(view7.png)";
+        document.body.style.backgroundImage = "url(view1.png)";
         document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundSize = "1670px 1600px";
-        document.body.style.backgroundPosition = "top right";
+        document.body.style.backgroundSize = "800px 600px";
+        document.body.style.backgroundPosition = "500px 400px";
+
+        let sendButton = document.getElementById("sendButton");
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox");
+
+        sendButton.addEventListener("click", function(event)
+        {
+            //event.preventDefault(); // right now for testing only
+
+            if(subscribeCheckbox.checked)
+            {
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                if(contact.serialize())
+                {
+                    let key = contact.FullName.substring(0, 1) + Date.now();
+
+                    localStorage.setItem(key, contact.serialize());
+                }
+            }
+        });
+    }
+
+    function DisplayContactListPage()
+    {
+        if(localStorage.length > 0)
+        {
+            let contactList = document.getElementById("contactList");
+
+            let data = "";
+
+            let keys = Object.keys(localStorage); // returns a list of keys from localStorage
+
+            let index = 1;
+
+            // for every key in the keys string array
+            for(const key of keys)
+            {
+                let contactData = localStorage.getItem(key); // get localStorage data value
+
+                let contact = new Contact(); // create an empty Contact object
+                contact.deserialize(contactData);
+
+                data += `<tr>
+                <th scope="row" class="text-center">${index}</th>
+                <td>${contact.FullName}</td>
+                <td>${contact.ContactNumber}</td>
+                <td>${contact.EmailAddress}</td>
+                <td></td>
+                <td></td>
+                </tr>`;
+                
+                index++;
+            }
+
+            contactList.innerHTML = data;
+        }
     }
 
 
@@ -123,6 +159,9 @@
                 break;
             case "Contact Us":
                 DisplayContactPage();
+                break;
+            case "Contact-List":
+                DisplayContactListPage();
                 break;
         }
        
